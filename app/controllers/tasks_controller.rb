3f8b935c638b0b4488e53d54f254aafb634d7694
hinakawa <wsk.ankit1k@gmail.com>
2020-08-31 09:48:51 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
