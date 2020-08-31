ec165500c9b0f65fd5e7c98c3ff194652ca81539
hinakawa <wsk.ankit1k@gmail.com>
2020-08-31 07:55:18 +0000

    crud

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'タスクを追加しました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクは追加できませんでした。'
+      render :new
+    end
+    
+  end 
+  def new
+    @task = Task.new
+  end 
+  def show
+    @task = Task.find(params[:id])
+  end 
+  def edit
+    @task = Task.find(params[:id])
+  end 
+  def update
+    @task = Task.find(params[:id])
+    if @task.update
+      flash[:success] = 'タスクを更新しました。'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクの更新に失敗しました。'
+      render :edit
+    end 
+      
+  end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] = 'タスクを削除しました。'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).parmit(:content)
+  end
+  
+end
