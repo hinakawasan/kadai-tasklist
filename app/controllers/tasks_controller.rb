8db927a880d9faf0f2350534b7ff83859df3d979
hinakawa <wsk.ankit1k@gmail.com>
2020-09-09 08:57:52 +0000

    fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = current_user.tasks.find_by(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
   end
   
   def task_params
